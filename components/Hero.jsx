c527c8d9627871c54bb219992220297fa6ef37c9
RaadC <jannradley.maniago@gsfe.tupcavite.edu.ph>
2022-12-03 12:07:44 +0800

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,21 @@
-import styles from "./components/Hero.module.css";
+import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
     <div className={styles.container}>
       <div className={styles.wrap}>
-        <div className={styles.sliding}></div>
+        <div className={styles.sliding}>
+        </div>
       </div>
       <div className={styles.herobox}>
-
-        
+        <p className={styles.title}>Welcome to Weebappp</p>
+        <p className={styles.subtitle}>‘Create and share your own stories and illustrations’</p>
+        <a href="#">
+          <button>Get Started</button>
+        </a>
       </div>
+      
+
     </div>
   );
 }
