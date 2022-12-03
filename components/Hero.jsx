71b5e1aebf65d9a442e0d8e84dc11b96e583936a
RaadC <jannradley.maniago@gsfe.tupcavite.edu.ph>
2022-12-03 09:16:32 +0800

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,15 @@
+import styles from "./components/Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrap}>
+        <div className={styles.sliding}></div>
+      </div>
+      <div className={styles.herobox}>
+
+        
+      </div>
+    </div>
+  );
+}
---
+++ pages/weebapp1.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Hero from "../components/Hero";
+
+export default function Weebapp(){
+    return(
+        <div>
+            <Hero />
+        </div>
+    );
+}
